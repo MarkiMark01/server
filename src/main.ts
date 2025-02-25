1fc36e7cfea718eb28373016539362dae65d1f4e
MarkiMark01 <markishgroup@gmail.com>
2025-02-25 16:08:07 +0100

    Fixed CORS issue

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173', 
+    origin: 'https://your-frontend-app.vercel.app', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
