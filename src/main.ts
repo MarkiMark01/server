36581c65438489bb4824ed3f702f369ad2066d00
MarkiMark01 <markishgroup@gmail.com>
2025-02-25 16:49:03 +0100

    Fixed main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://server-y0l4.onrender.com', 
+    origin: 'https://blogapp-sandy-mu.vercel.app', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
